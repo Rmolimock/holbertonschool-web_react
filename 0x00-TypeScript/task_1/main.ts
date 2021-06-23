01d5f320d4280b474457bbb5d4fae40166750bd8
Russell Molimock <38747720+Rmolimock@users.noreply.github.com>
2021-06-22 23:34:43 -0500

    Update main.ts

--- 0x00-TypeScript/task_1/main.ts
+++ 0x00-TypeScript/task_1/main.ts
@@ -1,8 +1,50 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
     [propName: string]: any;
+}
+
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface showTeachers {
+    (firstName: string, lastName: string): string;
+}
+
+export const showTeacher: showTeachers = (firstName:string, lastName: string) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface is {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface isc {
+    new (firstName: string, lastName: string): is;
+}
+
+export const StudentClass: isc = class StudentClass implements is{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return "Currently working"
+    }
+
+    displayName() {
+        return this.firstName
+    }
 }
